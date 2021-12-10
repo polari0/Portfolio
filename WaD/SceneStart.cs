4c564dd87380fc78e8017d5a4861a56dc31b637c
Polari <ilmari.vainio01@gmail.com>
2021-12-10 15:54:20 +0200

    Update SceneStart.cs

--- WaD/SceneStart.cs
+++ WaD/SceneStart.cs
@@ -6,19 +6,6 @@ namespace AngelTears
 {
     public class SceneStart : MonoBehaviour
     {
-        //    private void OnTriggerEnter2D(Collider2D collision)
-        //    {
-        //        print("Yes");
-        //    }
-        //    private void OnTriggerStay2D(Collider2D collision)
-        //    {
-        //        if (Input.GetKeyDown("e"))
-        //        {
-        //            print("Yes");
-        //        }
-        //    }
-        //}
-        //Mostly my failed aptempts while i tested all this out
         [SerializeField]
         private Spawner spawner_Script;
         private bool hasPlayerTouched = false;
@@ -26,14 +13,6 @@ namespace AngelTears
         private Demon demon_Script;
         //Serialized field is needed for to call Gamestart function when you press e to start the game. 
         //Bool checks wether or not you have touched the tv so you cant start the scene from other side of the map
-
-
-        //private void Start()
-        //{
-        //    gameStart = GetComponent<Spawner>();
-        //}
-        //This was somethin i tried it did not work out but lets leave it here for now
-
 
         private void OnTriggerEnter2D(Collider2D collision)
         {
@@ -56,12 +35,10 @@ namespace AngelTears
             {
                 print("e is pressed");
                 spawner_Script.StartGameTears();
-                //StartCoroutine(demon_Script.StartPitchFork());
                 demon_Script.hasGameStarted = true;
             }
         }
         //Once you have touched the TV pressing e will start the scene this piece of code will handle that
     }
-}
-//Sami get me some place holder sprites. 
+} 
 
