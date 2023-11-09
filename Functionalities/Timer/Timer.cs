a722a19e48d49dd093c838105f1ebf2754d0e057
polari0 <63444628+polari0@users.noreply.github.com>
2023-11-09 10:25:13 +0200

    Updated timer to have minute functionality

--- Functionalities/Timer/Timer.cs
+++ Functionalities/Timer/Timer.cs
@@ -1,60 +1,85 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using Unity.Mathematics;
 
-namespace AA2763
-{
-	public class Timer : MonoBehaviour
-	{
-		[SerializeField]
-		private TextMeshProUGUI timerText;
+public class Timer : MonoBehaviour
+ {
+    [SerializeField]
+    private TextMeshProUGUI timerText;
 
-		[Header("Timer settings")]
-		public float currentTime;
-		public bool countDown;
+    [Header("Timer settings")]
+    public float currentTime;
+    public bool countDown;
 
-		[Header("Limit Setting")]
-		public bool hasLimit;
-		public float timerLimit;
+    [Header("Limit Setting")]
+    public bool hasLimit;
+    public float timerLimit;
 
-		[Header("FormatSettings")]
-		public bool hasFormat;
-		public TimerFormats formats;
-		public Dictionary<TimerFormats, string> timerFormats = new Dictionary<TimerFormats, string>();
-		// Start is called before the first frame update
-		void Start()
-		{
-			timerFormats.Add(TimerFormats.Whole, "0");
-			timerFormats.Add(TimerFormats.TenthDecimal, "0.0");
-			timerFormats.Add(TimerFormats.HundrathsDecimal, "0.00");
-		}
+    [Header("FormatSettings")]
+    public bool isSecondsTimer;
+    public TimerFormats formats;
+    public Dictionary<TimerFormats, string> timerFormats = new Dictionary<TimerFormats, string>();
 
-		// Update is called once per frame
-		void Update()
-		{
-			currentTime = countDown ? currentTime -= Time.deltaTime : currentTime += Time.deltaTime;
+    private bool timeOver = true;
 
-			if (hasLimit && ((countDown && currentTime <= timerLimit) || (!countDown && currentTime >= timerLimit)))
-			{
-				currentTime = timerLimit;
-				SetTimerText();
-				timerText.color = Color.red;
-				enabled = false;
-			}
 
-			SetTimerText();
-		}
+    void Start()
+    {
+            timerFormats.Add(TimerFormats.Whole, "0");
+            timerFormats.Add(TimerFormats.TenthDecimal, "0.0");
+            timerFormats.Add(TimerFormats.HundrathsDecimal, "0.00");
+    }
 
-		private void SetTimerText()
-		{
-			timerText.text = hasFormat ? currentTime.ToString(timerFormats[formats]) : currentTime.ToString();
-		}
-	}
-	public enum TimerFormats
-	{
-		Whole,
-		TenthDecimal,
-		HundrathsDecimal
-	}; 
+
+    void Update()
+    {
+
+        currentTime = countDown ? currentTime -= Time.deltaTime : currentTime += Time.deltaTime;
+
+        if (hasLimit && ((countDown && currentTime <= timerLimit) || (!countDown && currentTime >= timerLimit)))
+        {
+            currentTime = timerLimit;
+            SetTimerText();
+            timerText.color = Color.red;
+            enabled = false;
+        }
+        else if (hasLimit && ((countDown && currentTime >= timerLimit) || (!countDown && currentTime <= timerLimit)))
+        {
+            if (timeOver)
+            {
+                TimeOver();
+                timeOver = false;
+            }
+        }
+
+        SetTimerText();
+    }
+
+
+    private void SetTimerText()
+    {
+        if (!isSecondsTimer)
+        {
+            float minutes = Mathf.FloorToInt(currentTime / 60);
+            float second = Mathf.FloorToInt(currentTime % 60);
+
+            timerText.text = string.Format("{0:00} : {1:00}", minutes, second);
+        }
+        else
+            timerText.text = isSecondsTimer ? currentTime.ToString(timerFormats[formats]) : currentTime.ToString();
+    }
+
+
+    private void TimeOver()
+    {
+        Debug.Log("TimeOver");
+    }
 }
+public enum TimerFormats
+{
+    Whole,
+    TenthDecimal,
+    HundrathsDecimal,
+};
